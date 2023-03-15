a56fc0dea4f5f05576ed1892c5d0a61ac940c21e
samssjubair <samssjubair@gmail.com>
2023-03-15 17:08:15 +0600

    mongoose schema created with get and post

---
+++ server.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose');
+const dotenv=require('dotenv').config();
+const colors =require('colors');
+
+const app=require('./app');
+
+mongoose.connect(process.env.DB_LOCAL).then(()=>{
+    console.log(`DB connection established`.red.bold);
+})
+
+const port= process.env.PORT || 8080;
+
+app.listen(port,()=>{
+    console.log(`App is running on port ${port}`.yellow.bold)
+})
+
